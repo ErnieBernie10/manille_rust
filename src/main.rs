354d1694b2fa4f2eeb106cbec0ef223e96cd0fa6
ErnieBernie10 <arne.boedt1@gmail.com>
2020-06-01 20:18:09 +0200

    Added game generator

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,13 @@ mod game;
 use game::table::Game;
 
 fn main() {
-    let _g = Game::new();
+    let games: Vec<Game> = create_games(10000);
+}
+
+fn create_games(amount: i32) -> Vec<Game> {
+    let mut games = Vec::<Game>::new();
+    for i in 0..amount {
+        games.push(Game::new());
+    }
+    games
 }
